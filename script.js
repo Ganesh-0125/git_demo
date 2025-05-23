d7b5cdf24a40a03b42d14e8cc7ca21bf080a7b78
Ganesh-0125 <ganeshchilukuri2001@gmail.com>
2025-05-23 11:43:56 +0530

    branch 1 first commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 <script>
     console.log("Hello, World!");
     const function = () =&gt;  { console.log("This is a function") };
+    const a =5;
+    const b = 10;
+    const sum = (a, b) =&gt; a + b;
+    console.log(sum(a, b));
 </script>
